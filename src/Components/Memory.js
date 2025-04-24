d76ba7c72a1e63386cd360e37e42f48d6fdd926b
Esha Patel <Patellesha11@gmail.com>
2025-04-24 20:52:58 +0300

    added const cards

--- src/Components/Memory.js
+++ src/Components/Memory.js
@@ -5,4 +5,13 @@ const images = ["🍕", "🍫", "🍇", "🫧", "🌷", "🦚", "🏏", "🐬"];
 const shuffled = [...images, ...images].sort(() => Math.random() - 0.5);
 
 function Game() {
+    const [cards, setCards] = useState(
+        shuffled.map((item, index) => ({
+          id: index,
+          value: item,
+          isFlipped: false,
+          isMatched: false,
+        }))
+      );
+      const [flipped, setFlipped] = useState([]);
 }
